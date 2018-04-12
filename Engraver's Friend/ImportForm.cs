52053b3864099d17641d0e35a8c714f659c891b3
JBoothIT <JBoothIT@users.noreply.github.com>
2018-04-11 19:45:07 -0500

    Initial commit

---
+++ Engraver's Friend/ImportForm.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Engraver_s_Friend
+{
+    /*
+    * Application Author: Jeremy Booth
+    * Company of Ownership: Bell Company, Inc.
+    * This application is the property of Bell Company, Inc and it's subsidiaries.
+    * It is not to be used for any purposes not expressly defined by Bell Company, Inc.
+    * This application may not be reproduced, modified or redistributed without written approval from Bell Company, Inc.
+    * 
+    * Persons and Entities who may make changes to the function of this application, general or otherwise,
+    * who shall not be considered subject to the aforementioned terms of use are as follows:
+    * Bell Company, Inc.
+    * Neil H. Bell
+    * Virginia Bell
+    * Christopher Bell
+    * Burt Bagley
+    * Jeremy Booth
+    * 
+    * Bell Company, Inc reserves the right to modify these Terms of Use at any time for any reason. 
+    */
+    public partial class ImportForm : Form
+    {
+        public ImportForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SSImport_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SSBrowse_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FolderBrowserDialog f1 = new FolderBrowserDialog();
+                if (f1.ShowDialog() == DialogResult.OK)
+                {
+                    SSPATH.Text = f1.SelectedPath;
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void DBBrowse_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FolderBrowserDialog f2 = new FolderBrowserDialog();
+                if (f2.ShowDialog() == DialogResult.OK)
+                {
+                    DBPATH.Text = f2.SelectedPath;
+                }
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
---
+++ Engraver's Friend/Main.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Engraver_s_Friend
+{
+    /*
+     * Application Author: Jeremy Booth
+     */
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        //Functions to be performed at application load
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!File.Exists("PATH.txt") || File.ReadLines("PATH.txt").ToString() != null)
+                {
+                    //CreateFile f2 = new CreateFile();
+                    //f2.ShowDialog();
+                }
+                else
+                {
+                    
+                }
+            }
+            catch(FileNotFoundException)
+            {
+                CreateFile f2 = new CreateFile();
+                f2.ShowDialog();
+            }
+            
+
+            //Displays the application build version
+            VersionBox.Text = "Version: " + Application.ProductVersion.ToString();
+
+            //Setup for email placeholder
+            EmailInsBox.Text = "sample@bellcoinc.com";
+            EmailInsBox.ForeColor = Color.Gray;
+        }
+
+        //Menu Strip: Import Button - Opens ImportForm
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ImportForm f1 = new ImportForm();
+            f1.ShowDialog();
+        }
+
+        //Menu Strip: Exit Button
+        private void ExitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //Insert/Submit Button Functions
+        private void InsButton_Click(object sender, EventArgs e)
+        {
+            if(InsButton.Text == "INSERT")
+            {
+                CompanyBox.Visible = false;
+                TypeBox.Visible = false;
+                SizeBox.Visible = false;
+                MaterialBox.Visible = false;
+                BevBox.Visible = false;
+                EmailBox.Visible = false;
+                PhoneBox.Visible = false;
+                CompanyInsBox.Visible = true;
+                MatInsBox.Visible = true;
+                MatNumLabel.Visible = true;
+                SizeInsBox.Visible = true;
+                TypeInsBox.Visible = true;
+                BevInsBox.Visible = true;
+                PhoneInsBox.Visible = true;
+
+                EmailInsBox.Text = "sample@bellcoinc.com";
+                EmailInsBox.ForeColor = Color.Gray;
+
+                InsButton.Text = "SUBMIT";
+            }
+            else if(InsButton.Text == "SUBMIT")
+            {
+                CompanyBox.Visible = true;
+                TypeBox.Visible = true;
+                SizeBox.Visible = true;
+                MaterialBox.Visible = true;
+                BevBox.Visible = true;
+                EmailBox.Visible = true;
+                PhoneBox.Visible = true;
+                CompanyInsBox.Visible = false;
+                MatInsBox.Visible = false;
+                MatNumLabel.Visible = false;
+                SizeInsBox.Visible = false;
+                TypeInsBox.Visible = false;
+                BevInsBox.Visible = false;
+                PhoneInsBox.Visible = false;
+                InsButton.Text = "INSERT";
+            }
+            else
+            {
+                MessageBox.Show("A critical error has occurred!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+        }
+
+        //Email Box: Onclick placeholder is erased
+        private void EmailInsBox_Enter(object sender, EventArgs e)
+        {
+            if (EmailInsBox.Text == "sample@bellcoinc.com")
+            {
+                EmailInsBox.Text = "";
+                EmailInsBox.ForeColor = Color.Black;
+            }
+        }
+
+        //Email Box: If left blank placeholder is typed in textbox
+        private void EmailInsBox_Leave(object sender, EventArgs e)
+        {
+            if (EmailInsBox.Text == "")
+            {
+                EmailInsBox.Text = "sample@bellcoinc.com";
+                EmailInsBox.ForeColor = Color.Gray;
+            }
+        }
+
+        private void createToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CreateFile f3 = new CreateFile();
+            f3.ShowDialog();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutBox1 f1 = new AboutBox1();
+            f1.ShowDialog();
+        }
+    }
+}
